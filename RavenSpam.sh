033c7308ca54ea65903a3b9a46ee1b7826938506
MrR4VEN <58394324+MrR4VEN@users.noreply.github.com>
2019-12-30 03:34:57 +0700

    Add files via upload

---
+++ RavenSpam.sh
@@ -0,0 +1,84 @@
+clear
+toilet -f bigmono9 -F gay SDV SPAM
+sleep 1
+echo "\033[34;1m[+]\033[32;1m═══════════════════════════════\033[34;1m[+]"
+echo "\033[32;1mAuthor: ⇝͜͡✪D°S°T~ᴹ̷ʳ̷ᴿ̷ᵃ̷ᵛ̷ᵉ̷ⁿ✪࿐"
+echo "\033[37;1mFeat: Go Hack DST Officials"
+echo "\033[35;1mUnknown?:Orang TerTanfanXD"
+echo "\033[35;1mTeam : Dark Side Team"
+echo "\033[33;1mKontak Me :  081339633530"
+echo "\033[92mMrRaven CYBER Army"
+echo "\033[36;1mBlackHole Security Indonesia"
+echo "\033[34;1m[+]\033[32;1m═══════════════════════════════\033[34;1m[+]"
+
+date|lolcat
+     echo "\033[34;1m Spam Yang Tersedia "
+     echo "\033[36;1m   '(01)'JDID "
+     echo "\033[32m   '(02)'Telkomsel "
+     echo "\033[34m   '(03)'PHD.      "
+     echo "\033[35m   '(04)'MatahariMall"
+     echo "\033[33m   '(05)'Tokopedia"
+     echo "\033[36;1m '(06)Install Bahan"
+     echo "\033[31m   '(00)Keluar "
+echo "\033[32m kamu pilih yang mana?? "
+read ezz
+if [ $ezz = 01 ] || [ $ezz = 1 ]
+then
+clear
+echo "\033[31;1m"
+toilet -f mono12 -F gay "JDID"
+php jdid.php
+fi
+
+if [ $ezz = 02 ] || [ $ezz = 2 ]
+then
+clear
+echo "\033[36;1m"
+toilet -f  mono12 -F gay "TELKOMSEL"
+php telkomsel.php
+fi
+
+if [ $ezz = 03 ] || [ $ezz = 3 ]
+then
+clear 
+echo  "\033[39;1m"
+toilet -f mono12 -F metal "PHD"
+php phd.php
+fi
+
+if [ $ezz = 04 ] || [ $ezz = 4 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "MATAHARIMALL"
+php mataharimall.php
+fi
+
+if [ $ezz = 05 ] || [ $ezz = 5 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "TOKPED"
+php tokped.php
+fi
+
+if [ $ezz = 06 ] || [ $ezz = 6 ]
+then 
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "INFO"
+echo "Dibuat oleh "
+echo "MrR4VEN"
+echo "Version First"
+echo "Baon Cikadap(Officials Video)"
+echo ""
+fi
+
+if [ $ezz = 00 ] || [ $ezz = 0 ]
+then
+sleep 1
+echo "Bye Jonn" | lolcat
+sleep 1
+echo "Jangan Lupa Liat Baon Cikadap Di YT" | lolcat
+exit
+fi
